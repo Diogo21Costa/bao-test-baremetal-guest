184a65141de564d10a6e50a0d1d43ac694f4188d
Diogo Costa <diogoandreveigacosta@gmail.com>
2026-05-20 10:00:59 +0100

    fix(sources): enable the use of different main.c files
    Signed-off-by: Diogo Costa <diogoandreveigacosta@gmail.com>

--- src/sources.mk
+++ src/sources.mk
@@ -1,4 +1,8 @@
 ## SPDX-License-Identifier: Apache-2.0
 ## Copyright (c) Bao Project and Contributors. All rights reserved.
 
-src_c_srcs:= main.c
+src_c_srcs:=
+
+ifneq ($(APP_EXTERNAL_MAIN),1)
+src_c_srcs+= main.c
+endif
